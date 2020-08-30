3db079e41d91e2d53f9a4a02909153f20ecbbb30
unknown <zhihorka@inbox.rum>
2020-08-30 18:55:18 +0300

    Updates

---
+++ precache-manifest.83951437fea554e2be7dbf97c88cafe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8500278d53de449b4cf842deb290cb63",
+    "url": "/arcanite-test-task/index.html"
+  },
+  {
+    "revision": "9d6039539268db0287d9",
+    "url": "/arcanite-test-task/static/css/main.6a89f730.chunk.css"
+  },
+  {
+    "revision": "c84413e15b773554cdbe",
+    "url": "/arcanite-test-task/static/js/2.3c8fb1d7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/arcanite-test-task/static/js/2.3c8fb1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6039539268db0287d9",
+    "url": "/arcanite-test-task/static/js/main.bef25ddc.chunk.js"
+  },
+  {
+    "revision": "1155ea716f5e4dc448d7",
+    "url": "/arcanite-test-task/static/js/runtime-main.31d12a10.js"
+  }
+]);
